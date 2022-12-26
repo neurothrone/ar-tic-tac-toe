2f68853cb04e618abfaa42b5fdfe905b68f15160
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-27 00:08:31 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Zaid Neurothrone on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
